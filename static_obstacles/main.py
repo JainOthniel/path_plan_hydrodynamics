a48f6bc959490010dfa5b519fa9bc375e332d7ab
JainOthniel <jainramesh136@gmail.com>
2024-10-21 00:05:22 +0530

    manim_main.py added

--- static_obstacles/main.py
+++ static_obstacles/main.py
@@ -69,7 +69,7 @@ def main():
     U_robo_magn = np.linalg.norm(vel,ord=2,axis=0)
     tspan = (0,6_00_000)
     t_eval = np.arange(0,6_00_000,1)
-
+    traje = solve_ivp(velocity_robot, tspan, r_d, method='RK45', t_eval=t_eval, args=(param, mob_fun))
     
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection='3d')
